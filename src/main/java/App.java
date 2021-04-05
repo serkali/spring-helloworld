5c1ab89d685c3e8c7c007077ae2526e0b7901220
serkali88 <serkali88@gmail.com>
2021-04-05 20:56:21 +0400

    Java pre-project. Практическая задача 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld") ;
+        System.out.println(bean2.getMessage());
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getMessage());
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.getMessage());
+        boolean compared = bean ==bean2;
+        boolean compared2 = bean3 == bean4;
+        System.out.println(compared);
+        System.out.println(compared2);
+
+
     }
+
 }
